5ba7cd59274b245b8742215c308d247786ad3365
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-05-15 21:35:21 +0200

    feat(create customer) : add endpoint to create a new customer

--- src/main/java/com/filipedevs/api/customer/CustomerController.java
+++ src/main/java/com/filipedevs/api/customer/CustomerController.java
@@ -1,12 +1,12 @@
 package com.filipedevs.api.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping(path = "api/v1/customers")
 public class CustomerController {
 
@@ -19,5 +19,11 @@ public class CustomerController {
     @GetMapping
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @PostMapping
+    public void createCustomer(@RequestBody CreateCustomerRequest createCustomerRequest) {
+        log.info("Received request to create customer: {}", createCustomerRequest);
+        customerService.createCustomer(createCustomerRequest);
     }
 }
